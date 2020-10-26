33cb45d2160f0de4c52f0a98539fca08bb54e5a7
OhYee <oyohyee@oyohyee.com>
2020-10-26 15:06:49 +0800

    workflow: just call python3 not python3.8(#master)

--- check/check.crontab.bash
+++ check/check.crontab.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
-export PYTHONPATH="/usr/lib/python38.zip:/usr/lib/python3.8:/usr/lib/python3.8/lib-dynload:/home/ubuntu/.local/lib/python3.8/site-packages:/usr/local/lib/python3.8/dist-packages:/usr/lib/python3/dist-packages"
+
+#export PYTHONPATH="/usr/lib/python38.zip:/usr/lib/python3.8:/usr/lib/python3.8/lib-dynload:/home/ubuntu/.local/lib/python3.8/site-packages:/usr/local/lib/python3.8/dist-packages:/usr/lib/python3/dist-packages"
+
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 
 date '+%Y-%m-%d %H:%M:%S' >> $SHELL_FOLDER/log.log
-python3.8 $SHELL_FOLDER/check.py 1>>$SHELL_FOLDER/log.log 2>>$SHELL_FOLDER/error.log
+python3 $SHELL_FOLDER/check.py 1>>$SHELL_FOLDER/log.log 2>>$SHELL_FOLDER/error.log
